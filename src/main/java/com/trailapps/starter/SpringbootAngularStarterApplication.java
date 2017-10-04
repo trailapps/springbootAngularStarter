3980e370c1ecb115b1876b8641442aca8ba7652e
Criticalhead46 <criticalhead46@outlook.com>
2017-10-04 23:43:58 +0530

    first commit

---
+++ src/main/java/com/trailapps/starter/SpringbootAngularStarterApplication.java
@@ -0,0 +1,12 @@
+package com.trailapps.starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAngularStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootAngularStarterApplication.class, args);
+	}
+}
